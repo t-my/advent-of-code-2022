fce236e5bb6d0d722c8852af30b670a86acacbc0
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2022-12-04 01:03:39 +0200

    day 1

---
+++ src/day_1/core.clj
@@ -0,0 +1,15 @@
+(ns day-1.core
+  (:require [clojure.string :as str]))
+
+(defn split-to-groups [delimiter str] (str/split str delimiter))
+
+(defn split-by-newline [str] (map #(str/split % #"\n") str))
+
+(->>
+ (slurp "src/day_1/input.txt")
+ (split-to-groups #"\n\n")
+ (split-by-newline)
+ (map #(map read-string %))
+ (map #(reduce + %))
+ (sort)
+ (last))
